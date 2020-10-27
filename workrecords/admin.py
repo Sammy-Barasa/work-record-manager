01523d61cc8b26a3eb7e1f75ca105c2831c8d6ac
Sammy-Barasa <kesammybarasa@gmail.com>
2020-10-27 06:19:03 +0300

    updated admin.py

--- workrecords/admin.py
+++ workrecords/admin.py
@@ -2,8 +2,8 @@ from django.contrib import admin
 from workrecords.models import Work, RecordOfWork, PersonChoises, TypeOfWorkChoices
 
 # Register your models here.
-class WorkRecordAdmin(admin.ModelAdmin):
-    list_display = ['date', 'person', 'title', 'pages',
+class WorkAdmin(admin.ModelAdmin):
+    list_display = ['user','date', 'person', 'title', 'pages',
                     'number_of_words', 'expected_amount','cancelled','completed', 'paid', 'amount_received']
     ordering = ['date']
 
@@ -19,7 +19,7 @@ class PersonChoisesAdmin(admin.ModelAdmin):
 class TypeOfWorkChoicesAdmin(admin.ModelAdmin):
     list_display = ['user', 'work_type']
 
-admin.site.register(Work,WorkRecordAdmin)
+admin.site.register(Work,WorkAdmin)
 admin.site.register(RecordOfWork,RecordOfWorkAdmin)
 admin.site.register(PersonChoises, PersonChoisesAdmin)
 admin.site.register(TypeOfWorkChoices, TypeOfWorkChoicesAdmin)
