967adec87a5249828e798dbcfa2910515e060321
Sammy Barasa <60486335+Sammy-Barasa@users.noreply.github.com>
2020-11-04 02:17:27 +0300

    updated swagger url

--- WorkRecordManager/urls.py
+++ WorkRecordManager/urls.py
@@ -15,9 +15,31 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="WorkRecords API",
+        default_version='v1',
+        description="An API for work records",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="test@workrecords.local"),
+        license=openapi.License(name="Test License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('authentication.urls')),
-    path('work/', include('workrecords.urls'))
+    path('work/', include('workrecords.urls')),
+    path("swagger", schema_view.without_ui(
+        cache_timeout=0), name='schema-json'),
+    path("", schema_view.with_ui('swagger',
+                                 cache_timeout=0), name='schema-swagger-ui'),
+    path("redoc/", schema_view.with_ui('redoc',
+                                       cache_timeout=0), name='schema-redoc'),
 ]
