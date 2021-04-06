420f9e187dfe03d0b273a73f0e057f613130694d
Sammy Barasa <60486335+Sammy-Barasa@users.noreply.github.com>
2021-04-06 18:16:18 +0300

    required=False

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -9,7 +9,7 @@ from django.contrib.auth import get_user_model
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(
         max_length=60, min_length=4, write_only=True)
-    fcm_token = serializers.CharField(write_only=True)
+    fcm_token = serializers.CharField(write_only=True, required= False)
     class Meta:
         model = get_user_model()
         fields = ['email', 'username', 'password','fcm_token']
