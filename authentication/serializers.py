17876a80ee07bbc2527bcea18cebb823d6b58930
Sammy Barasa <60486335+Sammy-Barasa@users.noreply.github.com>
2021-04-07 00:39:21 +0300

    removed fcm-token

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -9,15 +9,15 @@ from django.contrib.auth import get_user_model
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(
         max_length=60, min_length=4, write_only=True)
-    fcm_token = serializers.CharField(write_only=True, required= False)
+#     fcm_token = serializers.CharField(write_only=True, required= False)
     class Meta:
         model = get_user_model()
-        fields = ['email', 'username', 'password','fcm_token']
-        extra_kwargs = {'fcm_token':{
-                                      'required':False,
-                                      'allow_null':True
-                                    }
-                       }
+        fields = ['email', 'username', 'password']
+#         extra_kwargs = {'fcm_token':{
+#                                       'required':False,
+#                                       'allow_null':True
+#                                     }
+#                        }
 
     def validate(self, attr):
         email = attr.get('email')
