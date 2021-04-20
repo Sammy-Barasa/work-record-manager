d9195eb90bb5c0cf98347f983aa136d29d6f2bee
Sammy-Barasa <kesammybarasa@gmail.com>
2021-04-20 21:15:13 +0300

    changed email settigs

--- WorkRecordManager/settings.py
+++ WorkRecordManager/settings.py
@@ -230,7 +230,7 @@ EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-DEFAULT_FROM_EMAIL ='From WorkRecordsManager'
+DEFAULT_FROM_EMAIL ='WorkRecordsManager'
 
 
 # django-course-header
